96b9c5dcff97a49dc47e1817865e8825332aa39e
Cong-The-Pencil-Coder <cong.pham234@gmail.com>
2018-10-31 18:57:42 -0600

    WebSocket using SignalR

---
+++ PersonalFit/SignalRConnection.js
@@ -0,0 +1,14 @@
+﻿$.connection.hub.start()
+    .done(function () {
+        console.log("It Worked");
+        $.connection.ChatHub.server.Annouce("Connected");
+        //$.hubConnection().createHubProxy("Chat").invoke('Annouce');
+    })
+    .fail(function () {
+        alert("ERROR!!");
+    });
+
+$.connection.ChatHub.client.annouce(message) = function ()
+{
+    alert(message);
+}
